aab9313b814ee09d76699dca96793e96a3a67b95
shambhavi <shambhavi272002@gmail.com>
2023-02-24 17:17:54 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { Header } from './components/Home/Header'
 import { Home } from './components/Home/Home'
 import { CareersPage } from './components/careers/CareersPage'
 import { Footer } from './components/Footer'
+import { AboutUs } from './components/about/AboutUs'
 import './Footer.css'
-import { SideNavBar } from './components/SideNavBar'
+// import { SideNavBar } from './components/SideNavBar'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SideNavBar/>
+      {/* <SideNavBar/> */}
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/careerspage" element={<CareersPage />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
